47998a82a019c036a764f7a74972281d481d3dc8
Mehak <khan2234mehak@gmail.com>
2026-08-14 23:26:23 +0530

    Set Flask app to run in debug mode always

--- sleepsense_ai/app (1).py
+++ sleepsense_ai/app (1).py
@@ -961,4 +961,4 @@ def initialize_database():
 initialize_database()
 
 if __name__ == '__main__':
-    app.run(debug=os.environ.get('FLASK_DEBUG', '0') == '1', host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=5000)
