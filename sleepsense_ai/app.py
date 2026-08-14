93874d895a85f5e4b00ff79e52ba192b9bcd5b77
Mehak <khan2234mehak@gmail.com>
2026-08-14 23:24:35 +0530

    Refactor database initialization and run settings

--- sleepsense_ai/app.py
+++ sleepsense_ai/app.py
@@ -958,6 +958,7 @@ def initialize_database():
         else:
             raise exc
 
+initialize_database()
+
 if __name__ == '__main__':
-    initialize_database()
-    app.run(debug=os.environ.get('FLASK_DEBUG', '0') == '1', host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=5000)
